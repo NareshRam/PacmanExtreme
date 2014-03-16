e09089cff15de4ca22bd97d54f11dc2474c8921c
Naresh Ram <nram12@student.aau.dk>
2014-03-16 16:44:18 +0100

    Pacman object added with simple movment NOT DONE
    Pacman sphere with a rigidbody and very simple code to have him
    continuous movement and left/right movement. Movement needs to be less
    smooth

--- Assets/Character.cs
+++ Assets/Character.cs
@@ -11,5 +11,14 @@ public class Character : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 	
+		transform.position += new Vector3(0, 0, 0.2f); //make Pacman continously move forward
+		
+		if(Input.GetKey(KeyCode.A)){
+			rigidbody.AddForce (new Vector3(-20.0f, 0, 0)); //when A is pressed Pacman will move left in a smooth motion
+		}
+		
+		if(Input.GetKey(KeyCode.D)){
+			rigidbody.AddForce (new Vector3(20.0f, 0, 0)); //when D is pressed Pacman will move right in a smooth motion
+		}
 	}
 }
