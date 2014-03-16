22ab1b34855fdcb8cde42de81471ea4dd45edae7
evian133 <mmertz12@student.aau.dk>
2014-03-16 18:00:13 +0100

    Scoreboard
    Added a folder for scripts, and created a scoreboard. The scoreboard
    script has been added to the main character (pacman). The scoreboard
    will count upwards the longer Pacman is alive.

---
+++ Assets/scripts/score.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// A SCOREBOARD THAT WILL COUNT UPWARDS THE LONGER THE PLAYER IS IN THE GAME
+
+public class score : MonoBehaviour {
+	
+	
+	public float timer; //timer variable declared
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+			timer += Time.deltaTime; //a timer 
+	}
+	
+	void OnGUI() {
+		
+		float seconds = timer%60; //it will count in seconds
+		
+		GUI.Label (new Rect (10,10,250,100), "Score: " + Mathf.RoundToInt(seconds)); //makes it appear on the screen
+		
+	}
+}
