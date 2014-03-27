61c63cba618bd203d6df658bea03caf6e0034cce
Naresh Ram <nram12@student.aau.dk>
2014-03-27 12:26:28 +0100

    Changed movement (NOT DONE)
    no longer WASD now controlled with arrow keys, but still broken. Also
    needs to have continuous movement.

--- Assets/Character.cs
+++ Assets/Character.cs
@@ -5,20 +5,29 @@ public class Character : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
-		transform.position += new Vector3(0, 0, 0.2f); //make Pacman continously move forward
-		
-		if(Input.GetKey(KeyCode.A)){
-			transform.position += new Vector3(-2.0f, 0, 0); //when A is pressed Pacman will move left
-		}
-		
-		if(Input.GetKey(KeyCode.D)){
-			transform.position += new Vector3(2.0f, 0, 0); //when D is pressed Pacman will move right
+
+		transform.rotation = Quaternion.Euler(0, 0, 0);
+
+		if(Input.GetKey(KeyCode("up") || KeyCode("down") || KeyCode("left") || KeyCode("right"))){
+			if(Input.GetKey(KeyCode("up"))){
+				transform.position += new Vector3(-0.2f, 0, 0); //when A is pressed Pacman will move left
+			}
+			
+			if(Input.GetKey(KeyCode("down"))){
+				transform.position += new Vector3(0.2f, 0, 0); //when D is pressed Pacman will move right
+			}
+			if(Input.GetKey(KeyCode("left"))){
+				transform.position += new Vector3(0, 0, 0.2f); //when A is pressed Pacman will move left
+			}
+			
+			if(Input.GetKey(KeyCode("right"))){
+				transform.position += new Vector3(0, 0, -0.2f); //when D is pressed Pacman will move right
+			}
 		}
 	}
 }
