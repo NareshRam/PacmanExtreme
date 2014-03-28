c28ed1d1ed7a36e3f5099b4a456c0a15a35f7c99
Amar Al-Samarri <amar_al_samarri@hotmail.com>
2014-03-28 01:18:00 +0100

    Movement fixed
    You can now move Pacman by using the arrows instead of W, A, S and D
    buttons

--- Assets/Character.cs
+++ Assets/Character.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class Character : MonoBehaviour {
 
+
+
+
 	// Use this for initialization
 	void Start () {
 
@@ -10,24 +13,22 @@ public class Character : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-
 		transform.rotation = Quaternion.Euler(0, 0, 0);
+		
 
-		if(Input.GetKey(KeyCode("up") || KeyCode("down") || KeyCode("left") || KeyCode("right"))){
-			if(Input.GetKey(KeyCode("up"))){
-				transform.position += new Vector3(-0.2f, 0, 0); //when A is pressed Pacman will move left
+		if(Input.GetKey(KeyCode.UpArrow)){
+				transform.position += new Vector3(-0.2f, 0, 0); //when up arrow is pressed Pacman will move left
 			}
 			
-			if(Input.GetKey(KeyCode("down"))){
-				transform.position += new Vector3(0.2f, 0, 0); //when D is pressed Pacman will move right
+		if(Input.GetKey(KeyCode.DownArrow)){
+				transform.position += new Vector3(0.2f, 0, 0); //when down arrow is pressed Pacman will move right
 			}
-			if(Input.GetKey(KeyCode("left"))){
-				transform.position += new Vector3(0, 0, 0.2f); //when A is pressed Pacman will move left
+		if(Input.GetKey(KeyCode.LeftArrow)){
+				transform.position += new Vector3(0, 0, -0.2f); //when left arrow is pressed Pacman will move left
 			}
 			
-			if(Input.GetKey(KeyCode("right"))){
-				transform.position += new Vector3(0, 0, -0.2f); //when D is pressed Pacman will move right
+			if(Input.GetKey(KeyCode.RightArrow)){
+				transform.position += new Vector3(0, 0, 0.2f); //when right arrow is pressed Pacman will move right
 			}
 		}
 	}
-}
