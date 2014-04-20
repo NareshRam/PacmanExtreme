6360370e58e5d979b10bc6890e5298a54945ba54
Maxim M. <mmertz12@student.aau.dk>
2014-04-20 15:14:43 +0200

    score cahnged

--- Assets/scripts/score.cs
+++ Assets/scripts/score.cs
@@ -6,23 +6,21 @@ using System.Collections;
 public class score : MonoBehaviour {
 	
 	
-	public float timer; //timer variable declared
+	public int points; //timer variable declared
 	// Use this for initialization
 	void Start () {
 	
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
-			timer += Time.deltaTime; //a timer 
+	void OnCollisionEnter(Collision other){
+		if(other.gameObject.name == "small_pebble"){
+			points += 10; 
+		}
 	}
 	
-	void OnGUI() {
-		
-		float seconds = timer%60; //it will count in seconds
-		
-		GUI.Label (new Rect (10,10,250,100), "Score: " + Mathf.RoundToInt(seconds)); //makes it appear on the screen
-		
+	void OnGUI(){
+		GUI.Label(new Rect(10,10,250,100), "Score: " + points); //drawing a rectangle to show you the score
 	}
+
+
 }
