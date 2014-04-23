9d8889a20c5be4ff3d44c78094bb25a735720dd5
Amar Al-Samarri <amar_al_samarri@hotmail.com>
2014-04-23 02:24:59 +0200

    Score balls fixed + Score GUI
    The player can now collect the score balls and GUI of Total score are
    added

---
+++ Assets/Score_Behaviour.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Score_Behaviour : MonoBehaviour {
+
+	
+	void OnTriggerEnter(Collider other) { //Making a Collider
+		Score_Controller.scoreCount++; // Score plus one if player hits the balls
+
+		Destroy (this.gameObject); //Destroy the ball when player hits it
+
+	}
+}
+	
+
---
+++ Assets/Score_Controller.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Score_Controller : MonoBehaviour {
+
+	public static int scoreCount = 0; //Score have to begin with zero
+
+	void OnGUI() //void to make GUI
+	{
+		string scoreText = "Total Score: " + scoreCount; //Text of GUI and total score
+		GUI.Box(new Rect(Screen.width - 150, 20, 130, 20), scoreText); //The placement of GUI
+
+
+
+	}
+
+}
