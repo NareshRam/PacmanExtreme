8eabe1a63003581068e78e478d6fa88b755f0346
Naresh Ram <nram12@student.aau.dk>
2014-04-27 21:58:32 +0200

    Ghost AI 1 Done, using NavMesh
    Added the first AI, with NavMesh, might need some more so they dont all
    follow pacman in the exact same way

--- Assets/scripts/Enemy.cs
+++ Assets/scripts/Enemy.cs
@@ -2,21 +2,43 @@
 using System.Collections;
 
 public class Enemy : MonoBehaviour {
-
+	
+	private NavMeshAgent nav;
+	
+	GameObject player;
+	public Transform playerTransform;
+	
+	//variables for emergency back-up ai
+	//	public float speed;
+	//	public float rotationSpeed;
+	//	public Transform player;
+	//	public Transform myTransform;
+	
+	//get player position
+	Vector3 position;
 	
 	// Use this for initialization
 	void Start () {
-		
+		nav = GetComponent<NavMeshAgent>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-
+	void Update (){
+		player = GameObject.FindWithTag ("Player");
+		playerTransform = player.transform;
+		position = playerTransform.position;
+		nav.SetDestination (position);
+		
+		//emergency back-up ai - Chases directly after pacman, doesnt care about walls
+		//		player = GameObject.FindWithTag ("Player").transform;
+		//		myTransform.rotation = Quaternion.Slerp(myTransform.rotation, Quaternion.LookRotation(player.position - myTransform.position), rotationSpeed * Time.deltaTime);
+		//		
+		//		myTransform.position += myTransform.forward * speed * Time.deltaTime;
+		
 	}
-	
+
 	void OnCollisionEnter(Collision other){
 		if(other.gameObject.name == "Pacman"){
-
 //			Destroy (gameObject); //destroying object immediatly after
 			Application.LoadLevel ("gameover");
 		}
