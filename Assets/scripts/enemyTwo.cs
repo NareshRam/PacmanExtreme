be74cb2512f939f57054995e324a81ca10e6e4bf
Maxim M. <mmertz12@student.aau.dk>
2014-04-28 17:58:41 +0200

    Orange AI added
    Random ai that changes destination randomly in an interval of 20 seconds

--- Assets/scripts/enemyTwo.cs
+++ Assets/scripts/enemyTwo.cs
@@ -1,13 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-//enemy #1 , the Chaser
-public class Enemy : MonoBehaviour {
-	
+//enemy #4, stupid AI
+public class enemyTwo : MonoBehaviour {
+
+	bool timerLoop = true;
+
+
 	private NavMeshAgent nav;
-	
 	GameObject player;
 	public Transform playerTransform;
+	public float randX;
+	public float randZ;
 	
 	//variables for emergency back-up ai
 	//	public float speed;
@@ -17,19 +21,35 @@ public class Enemy : MonoBehaviour {
 	
 	//get player position
 	Vector3 position;
+	Vector3 RandPosition;
 	
 	// Use this for initialization
 	void Start () {
+		gameObject.renderer.material.color = new Color (1f, .65f, 0f); //setting the values for the color orange
 		nav = GetComponent<NavMeshAgent>();
-		gameObject.renderer.material.color = Color.red;
+
+		StartCoroutine(MyWaitingTime());
+
+	}
+	IEnumerator MyWaitingTime()
+	{
+		while(timerLoop){
+			randX = Random.Range(-27.0f, 27.0f);
+			randZ = Random.Range(-26.4f, 28.0f);
+			//The time before it resets back to its original position
+			yield return new WaitForSeconds (20.0f);
+
+		}
 	}
 	
 	// Update is called once per frame
 	void Update (){
+		MyWaitingTime ();
 		player = GameObject.FindWithTag ("Player");
 		playerTransform = player.transform;
 		position = playerTransform.position;
-		nav.SetDestination (position);
+		RandPosition = new Vector3(randX, 0.5f, randZ);
+		nav.SetDestination (RandPosition);
 		
 		//emergency back-up ai - Chases directly after pacman, doesnt care about walls
 		//		player = GameObject.FindWithTag ("Player").transform;
@@ -38,15 +58,15 @@ public class Enemy : MonoBehaviour {
 		//		myTransform.position += myTransform.forward * speed * Time.deltaTime;
 		
 	}
-
+	
 	void OnCollisionEnter(Collision other){
 		if(other.gameObject.name == "Pacman"){
-//			Destroy (gameObject); //destroying object immediatly after
-//			Application.LoadLevel ("gameover");
-
-
-
-
+			//			Destroy (gameObject); //destroying object immediatly after
+			//			Application.LoadLevel ("gameover");
+			
+			
+			
+			
 		}
 	}
 }
