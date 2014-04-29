492f7f66cb0852177c83217f872c117761e22e04
Maxim M <mmertz12@student.aau.dk>
2014-04-29 16:35:54 +0200

    Gameover screen slightly modified

--- Assets/scripts/Finalscore.cs
+++ Assets/scripts/Finalscore.cs
@@ -4,18 +4,24 @@ using System.Collections;
 public class Finalscore : MonoBehaviour {
 	public int finalScore;
 
+
 	// Use this for initialization
 	void Start () {
 		finalScore = PlayerPrefs.GetInt(" ");
+
 	}
-	
+
+
 	// Update is called once per frame
 	void Update () {
 	
 	}
 	void OnGUI() {
+
 		string scoreText = "FINAL SCORE: " + finalScore;
-		GUI.Box(new Rect(Screen.width/2-50, Screen.height/2-5, 125 , 60), scoreText);
+		GUI.Box(new Rect(Screen.width/3, Screen.height/1.7f, 400 , 100), "<color=white><size=50>"+scoreText+"</size></color>");
+//		GUI.Box(new Rect(Screen.width/3, Screen.height/4, 400 , 100), "<color=red><size=50>"+"GAME OVER!" + "</size></color>"); // GUI LABEL FOR THE GAME OVER TEXT
+
 		}
 
 }
