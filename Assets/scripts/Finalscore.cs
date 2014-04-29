67ce9e562d6841e85f9df1874a5c3e927831ccc0
Maxim M. <mmertz12@student.aau.dk>
2014-04-29 17:09:53 +0200

    minor changes

--- Assets/scripts/Finalscore.cs
+++ Assets/scripts/Finalscore.cs
@@ -19,7 +19,7 @@ public class Finalscore : MonoBehaviour {
 	void OnGUI() {
 
 		string scoreText = "FINAL SCORE: " + finalScore;
-		GUI.Box(new Rect(Screen.width/3, Screen.height/1.7f, 400 , 100), "<color=white><size=50>"+scoreText+"</size></color>");
+		GUI.Box(new Rect(Screen.width/3, Screen.height/1.7f, 550 , 100), "<color=white><size=50>"+scoreText+"</size></color>");
 //		GUI.Box(new Rect(Screen.width/3, Screen.height/4, 400 , 100), "<color=red><size=50>"+"GAME OVER!" + "</size></color>"); // GUI LABEL FOR THE GAME OVER TEXT
 
 		}
